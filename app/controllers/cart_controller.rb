f82c4c9f0ad04d7d30bd19cfb49ab3c2dacac2c0
Jonathan Turner <jhturner11@gmail.com>
2018-04-26 11:07:11 -0400

    updated cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -34,6 +34,7 @@ class CartController < ApplicationController
 
   def checkout
   	line_items = current_order.line_items
+    @order = current_order
   	@order = update(user_id: current_user.id, subtotal: 0)
   	line_items.each do |line_item|
   		line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
